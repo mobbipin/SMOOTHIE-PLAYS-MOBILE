79be5677145591a05448f142594b5dde8e3fccbd
mobbipin <bipin@mobcreatives.com.np>
2024-12-02 23:57:15 +0545

    SETUP

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Smoothie Plays"),
+        ),
+        body: const Center(
+          child: Text("Hello World! Welcome to Smoothie Plays"),
+        ),
+      ),
+    );
+  }
+}
