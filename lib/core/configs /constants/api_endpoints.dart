819e4dfa7ed0f4735a59d5dbca746a661b6bb3b4
mobbipin <bipin@mobcreatives.com.np>
2025-02-13 20:34:51 +0545

    NEW API ENDPOINTS

--- lib/core/configs /constants/api_endpoints.dart
+++ lib/core/configs /constants/api_endpoints.dart
@@ -1,7 +1,6 @@
 class ApiEndpoints {
   static const String baseUrl = 'http://localhost:5001/api';
-
-  // Auth Endpoints
-  static String login = '$baseUrl/auth/login';
-  static String signup = '$baseUrl/auth/signup';
+  static const String login = '$baseUrl/auth/login';
+  static const String signup = '$baseUrl/auth/signup';
+  static const String googleCallback = '$baseUrl/auth/google/callback';
 }
