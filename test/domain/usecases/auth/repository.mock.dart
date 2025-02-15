109a53070d13be8f6d772fd309ca8143b0d9697f
mobbipin <bipin@mobcreatives.com.np>
2025-02-15 19:16:47 +0545

    TTEST

--- test/domain/usecases/auth/repository.mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:smoothie_plays_mobile/domain/repository/auth/auth_repository.dart';
-
-class MockAuthRepository extends Mock implements AuthRepository {}
